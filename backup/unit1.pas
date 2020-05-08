e5566ff9cc9f264510cc132cb3d83d2719e4c5fc
HarreAtGitHUB <64368596+HarreAtGitHUB@users.noreply.github.com>
2020-05-08 23:19:09 +0200

    Sync
    Sync mit pool.ntp.de

---
+++ backup/unit1.pas
@@ -0,0 +1,50 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, IdDayTimeUDP,
+  IdDayTime, IdSNTP;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Label1: TLabel;
+    procedure Button1Click(Sender: TObject);
+
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+ uses sntpsend;
+{$R *.lfm}
+
+ { TForm1 }
+
+ procedure TForm1.Button1Click(Sender: TObject);
+ var
+   NTP: TIdSNTP;
+ begin
+   NTP := TIdSNTP.Create(nil);
+   try
+     NTP.Host := 'de.pool.ntp.org';
+     NTP.Active := True;
+     Label1.Caption := DateTimeToStr(NTP.DateTime);
+   finally
+     NTP.Free;
+end;
+
+
+end;
+
